8ba799a18f9bc42a59b8b84c3161621f751ae550
borginer <ariel.itskovich.il@gmail.com>
2024-03-18 17:39:27 +0200

    part2 ili_handler + ili_utils start

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -2,6 +2,60 @@
 
 .text
 .align 4, 0x90
+
+.macro push_caller_saved
+	pushq %rax
+	pushq %rdi
+	pushq %rsi
+	pushq %rdx
+	pushq %rcx
+	pushq %r8
+	pushq %r9
+	pushq %r10
+	pushq %r11
+.endm
+
+.macro pop_caller_saved
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq %rcx
+	popq %rdx
+	popq %rsi
+	popq %rdi
+	popq %rax
+.endm
+
 my_ili_handler:
-  ####### Some smart student's code here #######
+  push_caller_saved
+  movw 72(%rsp), %ax # get opcode after pushing regs
+
+  cmpb $0x0F, %al
+  jnz one_byte_opcode_HW3
+  shr $8, %rax
+one_byte_opcode_HW3:
+  movb %al, %dil
+
+  call what_to_do
+
+  cmpl $0, %eax
+  jnz normal_return_HW3
+
+call_old_handler_HW3:
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq %rcx
+	popq %rdx
+	popq %rsi
+	popq %rdi
+  movq %rax, %rdi # can't use my macro :(
+  popq %rax
+  jmp old_ili_handler
+
+normal_return_HW3:
+  pop_caller_saved
+  addq $4, (%rsp)
   iretq
--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -9,17 +9,21 @@ void my_store_idt(struct desc_ptr *idtr) {
 void my_load_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
 
-// <STUDENT FILL>
+// <STUDENT FILL>f
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-// </STUDENT FILL>
+    gate->offset_low = (unsigned long)addr & 0xffff;
+    gate->offset_middle = ((unsigned long)addr >> 16) & 0xffff;
+    gate->offset_high = ((unsigned long)addr >> 32) & 0xffffffff;
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+    unsigned long offset = 0;
 
-// </STUDENT FILL>
+    offset += gate->offset_low;
+    offset += (unsigned long)gate->offset_middle << 16;
+    offset += (unsigned long)gate->offset_middle << 32;
+
+    return offset;
 }
